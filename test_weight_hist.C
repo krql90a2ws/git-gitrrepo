f95b1f7a0765ba9c269089a9dae60e3ff6565347
Krishan Gopal <48828498+krql90a2ws@users.noreply.github.com>
2020-04-30 15:05:11 -0400

    check how git work (pull, push, clone)

---
+++ test_weight_hist.C
@@ -0,0 +1,18 @@
+void test_weight_hist(){
+
+  TH1D *hist = new TH1D("hist","hist",10,0,10);
+
+  Double_t x;
+   
+  for(int i =0;i<10;i++){
+    x = gRandom->Uniform(0,10);
+    hist->Fill(x,1);
+    cout<<hist->GetBinContent(i+1)<<endl;
+     
+  }
+  
+    hist->Draw("e");
+    cout<<hist->Integral()<<endl;
+    
+
+}
